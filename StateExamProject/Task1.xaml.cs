0ed7abf99c3aeac2f44efb11cab2fa5769d9df56
litov74dev <levt382@gmail.com>
2021-02-22 02:45:46 +0500

    Add project files.

---
+++ StateExamProject/Task1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StateExamProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Task1 : ContentPage
+    {
+        public Task1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public async void textInsertBtn_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textEntry.Text))
+            {
+                await DisplayAlert("Ошибка.", "Не введен текст!", "OK");
+            }
+            else
+            {
+                textLbl.Text = textEntry.Text;
+            }
+        }
+    }
+}
